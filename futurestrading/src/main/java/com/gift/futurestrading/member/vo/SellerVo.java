cb563b79704a64846cf226eadb5dda9118bd7d7a
devwondev <won3055@gmail.com>
2018-12-13 14:14:35 +0900

    [방혜원] 페이징 주석 추가
    1. Criteria 주석 추가
    
    2. PageMaker에서 페이징 관련 계산부분 주석 추가
    
    3. mapper, service, controller 정리

--- futurestrading/src/main/java/com/gift/futurestrading/member/vo/SellerVo.java
+++ futurestrading/src/main/java/com/gift/futurestrading/member/vo/SellerVo.java
@@ -1,4 +1,5 @@
 package com.gift.futurestrading.member.vo;
+
 public class SellerVo {
 	private String sellerIdPk;
 	private String sellerPassword;
@@ -13,75 +14,107 @@ public class SellerVo {
 	private String sellerAdmitAdmin;
 	private String sellerAdmitDate;
 	public String getSellerIdPk() {
+		System.out.println("SellerVo.getSellerIdPk() 호출");
 		return sellerIdPk;
 	}
 	public void setSellerIdPk(String sellerIdPk) {
+		System.out.println("SellerVo.setSellerIdPk() 호출");
 		this.sellerIdPk = sellerIdPk;
 	}
 	public String getSellerPassword() {
+		System.out.println("SellerVo.getSellerPassword() 호출");
 		return sellerPassword;
 	}
 	public void setSellerPassword(String sellerPassword) {
+		System.out.println("SellerVo.setSellerPassword() 호출");
 		this.sellerPassword = sellerPassword;
 	}
 	public String getSellerEmail() {
+		System.out.println("SellerVo.getSellerEmail() 호출");
 		return sellerEmail;
 	}
 	public void setSellerEmail(String sellerEmail) {
+		System.out.println("SellerVo.setSellerEmail() 호출");
 		this.sellerEmail = sellerEmail;
 	}
 	public String getSellerName() {
+		System.out.println("SellerVo.getSellerName() 호출");
 		return sellerName;
 	}
 	public void setSellerName(String sellerName) {
+		System.out.println("SellerVo.setSellerName() 호출");
 		this.sellerName = sellerName;
 	}
 	public String getSellerJuminNo() {
+		System.out.println("SellerVo.getSellerJuminNo() 호출");
 		return sellerJuminNo;
 	}
 	public void setSellerJuminNo(String sellerJuminNo) {
+		System.out.println("SellerVo.setSellerJuminNo() 호출");
 		this.sellerJuminNo = sellerJuminNo;
 	}
 	public String getSellerSignUpDate() {
+		System.out.println("SellerVo.getSellerSignUpDate() 호출");
 		return sellerSignUpDate;
 	}
 	public void setSellerSignUpDate(String sellerSignUpDate) {
+		System.out.println("SellerVo.setSellerSignUpDate() 호출");
 		this.sellerSignUpDate = sellerSignUpDate;
 	}
 	public String getSellerAddress() {
+		System.out.println("SellerVo.getSellerAddress() 호출");
 		return sellerAddress;
 	}
 	public void setSellerAddress(String sellerAddress) {
+		System.out.println("SellerVo.setSellerAddress() 호출");
 		this.sellerAddress = sellerAddress;
 	}
 	public String getSellerCorporateName() {
+		System.out.println("SellerVo.getSellerCorporateName() 호출");
 		return sellerCorporateName;
 	}
 	public void setSellerCorporateName(String sellerCorporateName) {
+		System.out.println("SellerVo.setSellerCorporateName() 호출");
 		this.sellerCorporateName = sellerCorporateName;
 	}
 	public String getSellerPhone() {
+		System.out.println("SellerVo.getSellerPhone() 호출");
 		return sellerPhone;
 	}
 	public void setSellerPhone(String sellerPhone) {
+		System.out.println("SellerVo.setSellerPhone() 호출");
 		this.sellerPhone = sellerPhone;
 	}
 	public String getSellerRight() {
+		System.out.println("SellerVo.getSellerRight() 호출");
 		return sellerRight;
 	}
 	public void setSellerRight(String sellerRight) {
+		System.out.println("SellerVo.setSellerRight() 호출");
 		this.sellerRight = sellerRight;
 	}
 	public String getSellerAdmitAdmin() {
+		System.out.println("SellerVo.getSellerAdmitAdmin() 호출");
 		return sellerAdmitAdmin;
 	}
 	public void setSellerAdmitAdmin(String sellerAdmitAdmin) {
+		System.out.println("SellerVo.setSellerAdmitAdmin() 호출");
 		this.sellerAdmitAdmin = sellerAdmitAdmin;
 	}
 	public String getSellerAdmitDate() {
+		System.out.println("SellerVo.getSellerAdmitDate() 호출");
 		return sellerAdmitDate;
 	}
 	public void setSellerAdmitDate(String sellerAdmitDate) {
+		System.out.println("SellerVo.setSellerAdmitDate() 호출");
 		this.sellerAdmitDate = sellerAdmitDate;
+	}
+	@Override
+	public String toString() {
+		return "SellerVo [sellerIdPk=" + sellerIdPk + ", sellerPassword=" + sellerPassword + ", sellerEmail="
+				+ sellerEmail + ", sellerName=" + sellerName + ", sellerJuminNo=" + sellerJuminNo
+				+ ", sellerSignUpDate=" + sellerSignUpDate + ", sellerAddress=" + sellerAddress
+				+ ", sellerCorporateName=" + sellerCorporateName + ", sellerPhone=" + sellerPhone + ", sellerRight="
+				+ sellerRight + ", sellerAdmitAdmin=" + sellerAdmitAdmin + ", sellerAdmitDate=" + sellerAdmitDate + "]";
 	}
 }
--- futurestrading/src/main/java/com/gift/futurestrading/page/vo/PageMaker.java
+++ futurestrading/src/main/java/com/gift/futurestrading/page/vo/PageMaker.java
@@ -1,15 +1,14 @@
 package com.gift.futurestrading.page.vo;
 
 public class PageMaker {
-	private int totalCount;     // 게시판 전체 데이터 개수
-	private int displayPageNum = 10;   // 게시판 화면에서 한번에 보여질 페이지 번호의 개수 ( 1,2,3,4,5,6,7,9,10)
-	
-	private int startPage;      // 현재 화면에서 보이는 startPage 번호
-	private int endPage;        // 현재 화면에 보이는 endPage 번호
-	private boolean prev;       // 페이징 이전 버튼 활성화 여부
-	private boolean next;       // 페이징 다음 버튼 활서화 여부
-	
-	private Criteria cri;       // 앞서 생성한 Criteria를 주입받는다.
+	private int totalCount;				/* 게시판 전체 데이터 개수 */
+	private int displayPageNum = 10;  	/* 게시판 화면에서 한번에 보여질 페이지 번호의 개수 (1,2,3,4,5,6,7,9,10) */
+	private int startPage;      		/* 현재 화면에서 보이는 startPage 번호 */
+	private int endPage;       			/* 현재 화면에 보이는 endPage 번호 */
+	private boolean prev;       		/* 페이징 이전 버튼 활성화 여부 */
+	private boolean next;      			/* 페이징 다음 버튼 활성화 여부 */
+
+	private Criteria cri;      /* Criteria를 주입받는다. */
 
 	public int getTotalCount() {
 		return totalCount;
@@ -22,19 +21,26 @@ public class PageMaker {
 	}
 	
 	private void calcData() {
+		
+		/* (int)(Math.ceil(현재 페이지번호 / (double)한번에 보여질 페이지 번호개수) * 한번에 보여질 페이지번호 개수). */
 		endPage = (int) (Math.ceil(cri.getPage() / (double) displayPageNum) * displayPageNum);
 		
+		/* (endPage - 한번에 보여질 페이지 번호 개수) + 1 */
 		startPage = (endPage - displayPageNum) + 1;
 		
+		/* (int)(Math.ceil(totalCount / (double)Criteria의 한페이지 당 게시글 개수)) */
 		int tempEndPage = (int) (Math.ceil(totalCount / (double) cri.getPerPageNum()));
 		
 		if(endPage > tempEndPage) {
 			endPage = tempEndPage;
 		}
 		
+		/* startPage가 1이 아니면 활성화, 1이 면 비활성화 */
 		prev = startPage == 1 ? false : true;
 		
+		/* (endPage * Criteria의 한페이지 당 게시글 개수) 가 totlaCount보다 크거나 같으면 비활성화, 아니면 활성화 */
 		next = endPage * cri.getPerPageNum() >= totalCount ? false : true;
+
 	}
 
 	public int getStartPage() {
