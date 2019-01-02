1fb04f30a1be2ba8c34137ae3401aed3c777c3fc
devwondev <won3055@gmail.com>
2019-01-02 16:04:27 +0900

    [방혜원] 로그인 전에 주문폼으로 넘어가지 않도록 OrderBuyController수정
    - OrderBuyController
    -> addConsumerOrder에서 조건문이용해서
    	session이 null일 경우, 경고창 띄움(로그인 후 이용가능합니다.)
    	session이 null이 아닐 때 주문폼으로 이동

--- futurestrading/src/main/java/com/gift/futurestrading/order/controller/OrderBuyController.java
+++ futurestrading/src/main/java/com/gift/futurestrading/order/controller/OrderBuyController.java
@@ -1,7 +1,11 @@
 package com.gift.futurestrading.order.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,26 +30,35 @@ public class OrderBuyController {
 	 * url 요청이 들어오면 구매자 주문페이지로 랜더링 해준다.
 	 * @param HttpSession session, Model model, OrderBuyVo orderBuyVo
 	 * @return order/consumer/addMemeberConsumerOrder
+	 * @throws IOException 
 	 * @since JDK1.8
 	 */
 	@RequestMapping(value="/consumer/add/order", method=RequestMethod.GET)
-	public String addConsumerOrder(@ModelAttribute("cri") Criteria cri, HttpSession session, Model model, OrderBuyVo orderBuyVo) {
+	public String addConsumerOrder(@ModelAttribute("cri") Criteria cri, HttpSession session, Model model, OrderBuyVo orderBuyVo, HttpServletRequest request, HttpServletResponse response) throws IOException {
 		System.out.println("OrderBuyController.addConsumerOrder() 호출");
-		model.addAttribute("sessionLogin", session.getAttribute("sessionLoginMember"));
+		/*로그인 전과 후로 나뉨*/
+		if(session.getAttribute("sessionLoginMember")==null) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>alert('로그인 후 이용가능합니다.'); history.go(-1);</script>");
+			out.flush();
+		}else {
+			/*회원별 리스트 가져옴 페이징완료*/
+			model.addAttribute("sessionLogin", session.getAttribute("sessionLoginMember"));
+			String sessionLoginId = (String) session.getAttribute("sessionLoginId");
+			List<OrderBuyListVo> getOrderBuyList = orderBuyService.getOrderBuyList(sessionLoginId, cri);
+			System.out.println(getOrderBuyList+"<--getOrderBuyList");
+			model.addAttribute("orderBuyList", getOrderBuyList);
+			PageMaker pageMaker = new PageMaker();
+			pageMaker.setCri(cri);
+			pageMaker.setTotalCount(orderBuyService.getOrderBuyAllConut(sessionLoginId));
+			model.addAttribute("pageMaker", pageMaker);
+			/*종가가져옴*/
+			int getClosingPriceResult = orderBuyService.getClosePriceItemOne();
+			model.addAttribute("closingPrice", getClosingPriceResult);
+			System.out.println(getClosingPriceResult+"<--getClosingPriceResult");	
+		}
 		
-		/*회원별 리스트 가져옴 페이징완료*/
-		String sessionLoginId = (String) session.getAttribute("sessionLoginId");
-		List<OrderBuyListVo> getOrderBuyList = orderBuyService.getOrderBuyList(sessionLoginId, cri);
-		System.out.println(getOrderBuyList+"<--getOrderBuyList");
-		model.addAttribute("orderBuyList", getOrderBuyList);
-		PageMaker pageMaker = new PageMaker();
-		pageMaker.setCri(cri);
-		pageMaker.setTotalCount(orderBuyService.getOrderBuyAllConut(sessionLoginId));
-		model.addAttribute("pageMaker", pageMaker);
-		/*종가가져옴*/
-		int getClosingPriceResult = orderBuyService.getClosePriceItemOne();
-		model.addAttribute("closingPrice", getClosingPriceResult);
-		System.out.println(getClosingPriceResult+"<--getClosingPriceResult");
 		return "order/consumer/addMemeberConsumerOrder";
 	}
 	/** 매수주문액션
